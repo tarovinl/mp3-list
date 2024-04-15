dca4885c562ac038a306ab93aaa4210cf3270a1e
tarovinl <kevcoraza@gmail.com>
2024-04-15 14:35:48 +0800

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+const tempMusicData = [
+  {
+    id: 1,
+    title: "Song 1",
+    artist: "Artist A",
+    genre: "Pop",
+  },
+  {
+    id: 2,
+    title: "Song 2",
+    artist: "Artist B",
+    genre: "Rock",
+  },
+  {
+    id: 3,
+    title: "Song 3",
+    artist: "Artist C",
+    genre: "Jazz",
+  },
+];
+const tempPlaylist = [
+  {
+    id: 1,
+    title: "Song 1",
+    artist: "Artist A",
+    genre: "Pop",
+  },
+  {
+    id: 2,
+    title: "Song 2",
+    artist: "Artist B",
+    genre: "Rock",
+  },
+  {
+    id: 3,
+    title: "Song 3",
+    artist: "Artist C",
+    genre: "Jazz",
+  },
+];
 
-function App() {
+function NavBar() {
+  const [query, setQuery] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="container">
+      <h1 style={{ textAlign: "center" }}>Music App</h1>
+      <input
+        className="search"
+        type="text"
+        placeholder="Search movies..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p>
+        {" "}
+        Found <strong>X</strong> results
+      </p>
+    </nav>
+  );
+}
+function App() {
+  const [musics, setMusic] = useState(tempMusicData);
+  const [playlist, setPlaylist] = useState(tempPlaylist);
+  const addToPlaylist = (music) => {
+    setPlaylist([...playlist, music]);
+  };
+  return (
+    <div>
+      <NavBar />
+      <div className="container">
+        <div className="container">
+          <h2>Music List</h2>
+
+          <ul>
+            {musics.map((music) => (
+              <li key={music.id}>
+                {music.title} by {music.artist} ({music.genre})
+                <button>♥️</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="container">
+          <h2>Playlist</h2>
+          <ul>
+            {playlist.map((music) => (
+              <li key={music.id}>
+                {music.title} by {music.artist}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
